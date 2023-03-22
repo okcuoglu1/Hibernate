493ab36d9f0c085dc63fe5e85a8f6e739d147ae9
Cemil Okcuoglu <okcuoglucemil@gmail.com>
2023-03-22 22:29:21 +0300

    Data save

---
+++ src/main/java/com/hb01/annotation/RunnerFetch01.java
@@ -0,0 +1,8 @@
+package com.hb01.annotation;
+
+public class RunnerFetch01 {
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/com/hb01/annotation/Student01.java
+++ src/main/java/com/hb01/annotation/Student01.java
@@ -1,19 +1,31 @@
 package com.hb01.annotation;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity //@Entity ile Bu sınıfın DB de bir tabloya karşılık gelmesini sağlıyoruz. student01
 @Table(name="t_student01") //Eğer tablo ismini customize @Table annotation kullanırız.
                           //DB de tablo isminin "t_student01" olarak değişmesini sağladı
-public class Student01 {
-    
 
+//Java kodu içinde bu class'a ulaşırken Student01 ile, SQL ile ulaşirken t_student01 ile yazmam lazım.
+public class Student01 {
+
+    //@Entity annotation koyuyorsak bu fieldlardan bir tanesi primary key olur.
+    @Id //->@Id annotation'u columnu primary key yapar.
+    //@Column(name="std_id") -> Column ismini customize eder.
     private int id;
 
+    @Column(name="student_name", length=100, nullable=false, unique = false) //->Bazı kısıtlamalar getiriyoruz. Özelleştiriyoruz.
     private String name;
 
+    //@Transient //->@Transient DB'deki tabloda grade isminde bir column oluşmasını engeller.
+    //Bazı fieldların DB ile iletişimi olmasın isteyebiliriz. O zaman @Transient kullanırız.
     private int grade;
+
+//    @Lob //->@Lob large object ile buyuk boyutlu data gelecek diyoruz.
+//    Hibernate'e large object diye bildiriyoruz.
+//    private byte[] image; -> image gibi yapılar diğerlerine kıyasla büyük mb kaplarlar o yüzden @Lob kullanırız.
+
+
 
         // !!! GETTER - SETTER
 
