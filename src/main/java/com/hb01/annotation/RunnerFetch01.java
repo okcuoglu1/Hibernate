26a6386ea086a217100f3c3401d5e24ebf418369
Cemil Okcuoglu <okcuoglucemil@gmail.com>
2023-03-24 15:47:02 +0300

    Recovery

--- src/main/java/com/hb01/annotation/RunnerFetch01.java
+++ src/main/java/com/hb01/annotation/RunnerFetch01.java
@@ -28,13 +28,13 @@ public class RunnerFetch01 {
 
         // !!! 1.Yol : get() ***********************************************
         //Gelen yapı SQL tarzında, ama get()'e verdiğimiz ilk parametreyle gelen record Student01 data türüne döndürür.
-//       Student01 student1 = session.get(Student01.class, 1001);
-//       Student01 student2 = session.get(Student01.class, 1002);
-//       Student01 student3 = session.get(Student01.class, 1003);
-//
-//       System.out.println(student1);
-//       System.out.println(student2);
-//       System.out.println(student3);
+        //Mappleme yaptık. Gelen record Student01 data türünde olacak dedik.
+//        Student01 student1 = session.get(Student01.class, 1001);
+//        Student01 student2 = session.get(Student01.class, 1002);
+//        Student01 student3 = session.get(Student01.class, 1003);
+//        System.out.println(student1);
+//        System.out.println(student2);
+//        System.out.println(student3);
 
        //get()'i kaç kere cagırırsak o kadar DB ye gidip SELECT sorgusu calıstırır.
         // Bir kere de yapılacak işi kaç kere get() cagırdıysak o kadar yapar.
@@ -46,9 +46,9 @@ public class RunnerFetch01 {
 
         // !!!  2.Yol : SQL *************************************************
 
-        //String sqlQuery = "SELECT * FROM t_student01";
+//        String sqlQuery = "SELECT * FROM t_student01";
 
-        //getResultList() recordları objeye cevirir(Javanın anlayacagı şekle).
+        //getResultList() birden fazla data geleceğini ve recordları da core Java'nın anlayacağı şekilde list yapısına atar.
         // List yapısında objeler getirir.Bu objelerin de fieldları geleceği icin array yapıda gelir.
         //Gelen her datayı alan Object[] datasını kullandık.
 //        List<Object[]> resultList = session.createSQLQuery(sqlQuery).getResultList();
@@ -58,26 +58,60 @@ public class RunnerFetch01 {
 
         // !!! 3.Yol : HQL ***************************************************
             // Trick : HQL sorgusunda From dan sonra  javadaki class ismi kullanilmali
-        String hqlQuery = "FROM Student01";
-        List<Student01> resultList2 = session.createQuery(hqlQuery, Student01.class).getResultList();
-        for (Student01 student01 : resultList2) {
-            System.out.println(student01);
-        }
+  //        String hqlQuery = "FROM Student01";
+        //createQuery(hqlQuery, Student01.class) ->Önce queryimi gönderiyoruz sonra mappliyeceği classı belirtiyoruz.
+        //HQL de data türünü mapplediğimiz icin object data type ile ugrasmamıza gerek yok.
+//          List<Student01> resultList2 = session.createQuery(hqlQuery, Student01.class).getResultList();
+//          for (Student01 student01 : resultList2) {
+//              System.out.println(student01);
+//          }
 
         // Not : yukardaki 3 metodu kiyaslayalim ...
-            // 1.hazir Metod 2. HQL 3. SQL
+            //KULLANIM SIRASI BEST PRACTICE ->> 1.hazir Metod 2. HQL 3. SQL
+        /*
+        KULLANIM SIRASI NEDEN?
+       1) 3'ü de aynı şeyi karşılıyorsa hazır methodları kullanırız. Çünkü SQL ve HQL Stringlerle çalışır ve hataya açıktır.
+       2) Hazır method yoksa 2.seçenek HQL. Çünkü projenin ilerki zamanlarında native sql türü değişebilir(msSQL,OracleSQL vb.)
+       HQL bu değişimlere hazırdır ve kod patlamaz. Ama biz postgreSQL ile yazdıgımız sorgu sonra update edildiğinde farklı bir
+       türe geçtiğimizde kod patlar.
+         */
+
+        //----------------------------------------------------------------
+
+        //Getireceğimiz data tek bir dataysa mesela id ile sorgu sağlayacaksak... getResultList'e gerek yok!
         // !!! Donecek kaydin unique ( tek bir tane ) oldugundan emin isem uniqueResult() ..
+
         // SQL ile
+        //mappleme yapamadığımız için object[] ile aldık.
+        //Gelen recordun hangi data typelarında olacağını bilemediğimiz için hata almamak için kendimizi garantiye alıyoruz. Object[] kullanıyoruz.
 //        String sqlQuery2 = "SELECT * FROM t_student01 WHERE student_name ='Cemil Bey'";
 //        Object[] uniqueResult1 = (Object[]) session.createSQLQuery(sqlQuery2).uniqueResult();
 //        System.out.println(Arrays.toString(uniqueResult1));
-//
-//        // yukarda 1 obje gelecek ama icinde fieldlar oldugu icin bunlarti ayri ayri almak istersem:
+
+        //Array almamızın bir diğer yararı da; icindeki fiedları ayrı ayrı almak istersek index verip alabiliriz.
+        // yukarda 1 obje gelecek ama icinde fieldlar oldugu icin bunlari ayri ayri almak istersem:
 //        System.out.println(uniqueResult1[0] + " : " + uniqueResult1[1] + " : " + uniqueResult1[2]);
-        // HQL ile
-//        String hqlQuery2 = "FROM Student01 WHERE name='Cemil Bey'";
-//        Student01 uniqueResult2 = session.createQuery(hqlQuery2, Student01.class).uniqueResult();
-//        System.out.println(uniqueResult2);
+
+
+     // HQL ile
+     //mappleme yapabildik. Gelen recordun data typeını bildiğimiz icin object e falan gerek yok. HQL avantajı.
+//       String hqlQuery2 = "FROM Student01 WHERE name='Cemil Bey'";
+//       Student01 uniqueResult2 = session.createQuery(hqlQuery2, Student01.class).uniqueResult();
+//       System.out.println(uniqueResult2);
+
+
+       //!!!YUKARDAKI sorguyu HQL alias(takma ad) kullanarak yapalım
+//        String hqlQuery3 = "FROM Student01 as std  WHERE std.name='Cemil Bey'";
+//        Student01 uniqueResult3 = session.createQuery(hqlQuery3, Student01.class).uniqueResult();
+//        System.out.println(uniqueResult3);
+
+        //!! HQL ile grade değeri 90 olan ogrencileri getirelim-
+        String hqlQuery4 = "SELECT s.id, s.name FROM Student01 s WHERE s.grade=90";
+        List<Object[]> resultList4 = session.createQuery(hqlQuery4).getResultList();
+        for(Object[] object : resultList4){
+            System.out.println(Arrays.toString(object));
+        }
+
 
 
         tx.commit();
