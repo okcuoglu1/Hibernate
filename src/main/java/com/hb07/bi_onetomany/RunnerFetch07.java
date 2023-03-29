bcaba19ccd9e7a2d8d6b76a9311d187d1e0e894b
Cemil Okcuoglu <okcuoglucemil@gmail.com>
2023-03-29 22:05:05 +0300

    one to many relation

---
+++ src/main/java/com/hb07/bi_onetomany/RunnerFetch07.java
@@ -0,0 +1,31 @@
+package com.hb07.bi_onetomany;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class RunnerFetch07 {
+    public static void main(String[] args) {
+
+        Configuration con = new Configuration().configure("hibernate.cfg.xml").
+                addAnnotatedClass(Student07.class).addAnnotatedClass(Book07.class);
+
+        SessionFactory sf = con.buildSessionFactory();
+        Session session =  sf.openSession();
+        Transaction tx = session.beginTransaction();
+
+        // !!! get
+        Student07 student = session.get(Student07.class,1001);
+        System.out.println(student);
+
+        // !!! SQL ile öğrenci bilgilerini ( Inner Join )  alalım ( öğrenci ismi ve kitap ismi )
+
+        // !!! yukardaki sorguyu HQL ile yazalım
+
+
+        tx.commit();
+        session.close();
+        sf.close();
+    }
+}
